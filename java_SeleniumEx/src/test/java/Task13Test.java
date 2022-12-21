880ac8fa47b12b1d0a0624665a58576bca43be1e
Rustem Berkaliev <rustem.berkaliev@gmail.com>
2022-12-22 02:45:55 +0300

    Task13 actions basket test

---
+++ java_SeleniumEx/src/test/java/Task13Test.java
@@ -0,0 +1,111 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Task13Test {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private final By PRODUCT_ITEM = By.cssSelector("li.product a.link");
+    private final By ADD_TO_CART = By.name("add_cart_product");
+    private final By SIZE = By.name("options[Size]");
+    private final By CHECKOUT = By.xpath("//a[contains(text(), 'Checkout')]");
+    private final By REMOVE_ITEM = By.name("remove_cart_item");
+    private final By TABLE_ROW = By.cssSelector(".dataTable tr");
+
+
+    @BeforeEach
+    public void start() {
+        driver = Config.startBrowser("chrome");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    @AfterEach
+    public void stop() {
+        driver.quit();
+    }
+
+    @Test
+    public void actionsBasketTest() {
+        driver.get("http://localhost/litecart");
+        addRandomItemToTheBasketStep();
+        checkItemsCounterUpdated("1");
+
+        driver.get("http://localhost/litecart");
+        addRandomItemToTheBasketStep();
+        checkItemsCounterUpdated("2");
+
+        driver.get("http://localhost/litecart");
+        addRandomItemToTheBasketStep();
+        checkItemsCounterUpdated("3");
+
+        driver.findElement(CHECKOUT).click();
+
+        checkRemoveItemsButton(3);
+    }
+
+    private void checkItemsCounterUpdated(String count) {
+        try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+            WebElement counter = driver.findElement(By.className("quantity"));
+            wait.until(ExpectedConditions.textToBePresentInElement(counter, count));
+        }
+        catch (TimeoutException e) {
+            Assertions.fail("Items counter was not updated");
+        }
+        finally {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        }
+    }
+
+    private void checkRemoveItemsButton(int count) {
+        while (count > 0) {
+            if (elementIsNotFound(TABLE_ROW) && elementIsNotFound(TABLE_ROW)) {
+                return;
+            }
+
+            int tableSizeBefore = driver.findElements(TABLE_ROW).size();
+            driver.findElement(REMOVE_ITEM).click();
+            count--;
+
+            wait.until(ExpectedConditions.numberOfElementsToBeLessThan(TABLE_ROW, tableSizeBefore));
+            int tableSizeAfter = driver.findElements(TABLE_ROW).size();
+            Assertions.assertTrue(tableSizeBefore > tableSizeAfter);
+        }
+    }
+
+    private void addRandomItemToTheBasketStep() {
+        List<WebElement> items = driver.findElements(PRODUCT_ITEM);
+        int index = (int)(Math.random() * (items.size()-1));
+        items.get(index).click();
+
+        if (!elementIsNotFound(SIZE)) {
+            WebElement size = driver.findElement(SIZE);
+            Select sizeSelector = new Select(size);
+            sizeSelector.selectByIndex(1);
+        }
+        driver.findElement(ADD_TO_CART).click();
+    }
+
+    private boolean elementIsNotFound(By locator) {
+        try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+            return driver.findElements(locator).size() == 0;
+        }
+        finally {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        }
+    }
+}
