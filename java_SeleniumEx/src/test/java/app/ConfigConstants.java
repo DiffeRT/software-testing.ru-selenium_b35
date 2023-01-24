ed87c123e1bc82485fd852835d228d8dc63286b0
Rustem Berkaliev <rustem.berkaliev@gmail.com>
2023-01-25 01:56:16 +0300

    Task 19 Extended

--- java_SeleniumEx/src/test/java/app/ConfigConstants.java
+++ java_SeleniumEx/src/test/java/app/ConfigConstants.java
@@ -6,6 +6,6 @@ public class ConfigConstants {
     public static Duration IMPLICITLY_WAIT = Duration.ofSeconds(5);
     public static String REMOTE_STANDALONE_SERVER = "192.168.248.131:4444";
     public static String REMOTE_HUB_SERVER = "192.168.248.131:4444";
-    public static String BRO_STACK_USER = "bsuser_uEPnNg";
-    public static String BRO_STACK_KEY = "PTSzrqPkF7dznPTGkLbx";
+    public static String BRO_STACK_USER = "***";
+    public static String BRO_STACK_KEY = "***";
 }
